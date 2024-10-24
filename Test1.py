d468a8b57985dde70e8d2b83dfdf1ce64efea619
BartDaemen <45167136+BartDaemen@users.noreply.github.com>
2024-10-24 16:47:01 +0200

    Delete Test1.py

--- Test1.py
+++
@@ -1,8 +0,0 @@
-print("deze code komt van Github en is een test")
-print("import gelukt")
-#code vanuit pycharm
-print("Deze code komt van pycharm")
-#verdere aanpassing
-print("tweede test 5")
-#tweede keer vanuit Github
-print("test SFSDQfjkmslqdfj")
