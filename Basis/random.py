8f288955491670b794898907c58f2ce3780ae0ab
BartDaemen <45167136+BartDaemen@users.noreply.github.com>
2024-10-30 20:22:46 +0100

    Create random.py

---
+++ Basis/random.py
@@ -0,0 +1,19 @@
+import random
+
+print(random.randrange(1, 10)) 
+
+# randint Return a number between 3 and 9 (both included):
+
+import random
+print(random.randint(3, 9)) 
+
+# Return a random element from a list:
+
+import random
+mylist = ["apple", "banana", "cherry"]
+print(random.choice(mylist)) 
+
+# Return random number between 0.0 and 1.0:
+
+import random
+print(random.random()) 
