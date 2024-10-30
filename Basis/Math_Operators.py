148d490195eff69c576a846d5ff99018db01bbb7
BartDaemen <45167136+BartDaemen@users.noreply.github.com>
2024-10-30 19:06:57 +0100

    Update Math_Operators.py

--- Basis/Math_Operators.py
+++ Basis/Math_Operators.py
@@ -1,29 +1,11 @@
 # Math Operators
 
-		# 		**		Exponent						2 ** 3 = 8
-		# 		%			Modulus/Remainder		22 % 8 = 6
+		# 		**		Exponent			2 ** 3 = 8
+		# 		%		Modulus/Remainder		22 % 8 = 6
 		# 		//		Integer division		22 // 8 = 2
-		# 		/			Division						22 / 8 = 2.75
-		# 		*			Multiplication			3 * 3 = 9
-		# 		-			Subtraction					5 - 2 = 3
-		# 		+			Addition						2 + 2 = 4
-
-
-# Data types
-
-	# integers		gehele getallen
-	# floats			kommagetallen
-	# strings			tekst
-
-
-
-
-
-# Variabelen
-
-	# variabele bestaat steeds uit één woord
-	# kan enkel letters, cijfers en underscores (_) bevatten
-	# kan niet met een cijfer beginnen
-
+		# 		/		Division			22 / 8 = 2.75
+		# 		*		Multiplication			3 * 3 = 9
+		# 		-		Subtraction			5 - 2 = 3
+		# 		+		Addition			2 + 2 = 4
 
 
