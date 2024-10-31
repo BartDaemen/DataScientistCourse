72d1e25ac0fe617e8a61b1a7708bdb3b3edf16d8
BartDaemen <45167136+BartDaemen@users.noreply.github.com>
2024-10-31 08:44:59 +0100

    Update Operators.py

--- Basis/Operators.py
+++ Basis/Operators.py
@@ -8,4 +8,42 @@
 		# 		-		Subtraction			5 - 2 = 3
 		# 		+		Addition			2 + 2 = 4
 
+# Comparison Operators
 
+	# ==	Equal	x == y	
+	# !=	Not equal	x != y	
+	# >	Greater than	x > y	
+	# <	Less than	x < y	
+	# >=	Greater than or equal to	x >= y	
+	# <=	Less than or equal to	x <= y
+
+# Logical Operators
+
+	# and 	Returns True if both statements are true	x < 5 and  x < 10	
+	# or	Returns True if one of the statements is true	x < 5 or x < 4	
+	# not	Reverse the result, returns False if the result is true	not(x < 5 and x < 10)
+
+# Membership Operators
+	
+	# in 	Returns True if a sequence with the specified value is present in the object	x in y	
+	# not in	Returns True if a sequence with the specified value is not present in the object	x not in y
+
+
+# Boolean Values
+
+	# Booleansrepresent one of two values: True or False.
+
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+# meeste waarden zijn True
+
+	# Almost any value is evaluated to True if it has some sort of content.
+	# Any string is True, except empty strings.
+	# Any number is True, except 0.
+	# Any list, tuple, set, and dictionary are True, except empty ones.
