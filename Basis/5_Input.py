fbe759e00e5fa1eda1a694ddb7ac60d4b337b7e5
BartDaemen <45167136+BartDaemen@users.noreply.github.com>
2024-11-05 07:54:04 +0100

    Update 5_Input.py

--- Basis/5_Input.py
+++ Basis/5_Input.py
@@ -3,3 +3,6 @@
 
 username = input("Enter username:")
 print("Username is: " + username)
+
+# input slaat steeds op als string, wil je omzetten gebruik dan bijv. 
+getal = int(input("Geef getal in"))
